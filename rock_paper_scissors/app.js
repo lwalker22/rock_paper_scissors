80938794e0cffd2eec065d8b7a2b3f9957637eff
Logan Walker <loganwalker22@gmail.com>
2020-06-14 11:11:43 -0600

    displays user and comp choice

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -2,6 +2,7 @@
 let rpsGame = ["rock", "paper", "scissors"];
 
 let randomChoice;
+
 function randomGuess() {
 randomChoice = Math.floor(Math.random()*rpsGame.length);
 
@@ -15,11 +16,11 @@ randomChoice = Math.floor(Math.random()*rpsGame.length);
     computerChoice = "scissors";
   }
 
-  //console.log(computerChoice);
 }
-
  // application state
  let userInput;
+
+ //document.getElementById("#userChoice").style.width = '300px'
 
  // elements selected
  let rock = document.querySelector("#rock");
@@ -30,6 +31,7 @@ randomChoice = Math.floor(Math.random()*rpsGame.length);
    userInput = "rock";
    randomGuess();
    playGame();
+   //userGameOutput();
 
  })
 
@@ -38,6 +40,7 @@ randomChoice = Math.floor(Math.random()*rpsGame.length);
    userInput = "paper";
    randomGuess();
    playGame();
+   //userGameOutput();
 
  })
 
@@ -46,7 +49,79 @@ randomChoice = Math.floor(Math.random()*rpsGame.length);
    userInput = "scissors";
    randomGuess();
    playGame();
+   //userGameOutput();
  })
+
+//  let userChoice = document.querySelector("#playerChoice");
+//  userChoice.addEventListener("click", (event) => {
+//    userGameOutput();
+
+document.getElementById('rock').addEventListener("click", userGameOutput)
+var changeImg = document.getElementById("userChoice")
+
+document.getElementById('paper').addEventListener("click", userGameOutput)
+var changeImg = document.getElementById("userChoice")
+
+document.getElementById('scissors').addEventListener("click", userGameOutput)
+var changeImg = document.getElementById("userChoice")
+
+document.getElementById('rock').addEventListener("click", computerGameOutput)
+var comChangeImg = document.getElementById("computerChoice")
+
+document.getElementById('paper').addEventListener("click", computerGameOutput)
+var comChangeImg = document.getElementById("computerChoice")
+
+document.getElementById('scissors').addEventListener("click", computerGameOutput)
+var comChangeImg = document.getElementById("computerChoice")
+
+   
+ function userGameOutput() {
+   if (userInput == "rock") {
+     changeImg.src = "./images/rock.jpg";
+   }
+   else if (userInput == "paper") {
+     changeImg.src = "./images/paper.jpg";
+   }
+   else {
+     changeImg.src = "./images/scissors.jpg";
+ }
+}
+
+function computerGameOutput() {
+  if (computerChoice == "rock") {
+    comChangeImg.src = "./images/rock.jpg";
+  }
+  else if (computerChoice == "paper") {
+    comChangeImg.src = "./images/paper.jpg";
+  }
+  else {
+    comChangeImg.src = "./images/scissors.jpg";
+}
+}
+
+// funciton userGameOutput() {
+//   if (userInput == "rock") {
+//     userChoice.src == "./images/rock.jpg";
+//   }
+//   else if (userInput == "paper") {
+//     userChoice.src = "./images/paper.jpg";
+//   }
+//   else {
+//     userChoice.src = "./images/scissors.jpg";
+//   }
+// }
+
+// document.getElementById("taco").style.width = '300px'
+// // event listeners
+// document.getElementById('btn').addEventListener("click", changePic)
+// var taco = document.getElementById("taco")
+// function changePic() {
+//   if (taco.src === "https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1314&q=80")
+//     taco.src = 'https://images.unsplash.com/photo-1508154048109-de555266b70a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'
+//   else {
+//     taco.src = "https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1314&q=80"
+//   }
+// }
 
 function playGame() {
   if (userInput == computerChoice) {
@@ -77,6 +152,8 @@ function playGame() {
 
   console.log(computerChoice);
 }
+
+
 
 
 function loseGame() {
